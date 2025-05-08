e8043391cf34627a41221c2d422e9e44beb61795
tiagolbiotech <tiagolbiotech@gmail.com>
2025-05-08 10:22:29 -0300

    new bmp script that does not repeat OTU IDs

--- dockerfiles/perl/bmp-otuName.pl
+++ dockerfiles/perl/bmp-otuName.pl
@@ -7,51 +7,53 @@
 #QiimeToUparse
 #How to use this script?
 #perl bmp-otuName.pl -i something.fasta -o something.uparse.fasta
+#!/bin/perl
+
 use strict;
 use warnings;
-use Getopt::Std;
 use Getopt::Long;
-
 
 my $inputfile;
 my $outputfile;
 
+Getopt::Long::Configure('bundling');
+GetOptions(
+    'i|input_file=s'         => \$inputfile,
+    'o|output_file_prefix=s' => \$outputfile
+);
 
-Getopt::Long::Configure ('bundling');
-GetOptions ('i|input_file=s' => \$inputfile,
-	    'o|output_file_prefix=s' => \$outputfile);
-
-if(!defined($inputfile)) {
-    die ("Usage: otuName.pl -i <input file> -o <output file>\n");
+if (!defined($inputfile)) {
+    die("Usage: otuName.pl -i <input file> -o <output file>\n");
 }
 
-if(!defined($outputfile)) {
-    die ("Usage: otuName.pl -i <input file> -o <output file>\n");
+if (!defined($outputfile)) {
+    die("Usage: otuName.pl -i <input file> -o <output file>\n");
 }
 
-
-$/ = ">"; 
+$/ = ">";
 
 open(FASTA, $inputfile) or die "Cannot open the fasta file...";
-my $f = $inputfile;
-$f =~ s/\..*$//g;
 open(SAIDA, ">" . $outputfile) or die "!";
-$_ = <FASTA>;
 
+$_ = <FASTA>;  # Skip the first empty record
+my %used_random_numbers;  # Hash to store used random numbers
+my $range = 1000000000;
 
-	    while(my $inputfile = <FASTA>){
-	chomp($inputfile);
-	(my $header, my $sequence) = split("\n", $inputfile);
-	(my $nome, my $campo02, my $campo3, my $campo4, my $barcode01, my $barcode02, my$campo6) = split(" ", $header);
-		my $novo_nome = $nome;
-	
-	$novo_nome =~ s/\_.*$//g;
-	
-	my $range = 1000000000;
-	my $random_number = int(rand($range));
-	
-	print SAIDA ">OTU$random_number\n$sequence\n";
-
+while (my $inputfile = <FASTA>) {
+    chomp($inputfile);
+    my ($header, $sequence) = split("\n", $inputfile, 2);
+    $sequence =~ s/\n//g;  # Remove any newline characters from the sequence
+    
+    my $random_number;
+    
+    do {
+        $random_number = int(rand($range));
+    } while (exists $used_random_numbers{$random_number});
+    
+    $used_random_numbers{$random_number} = 1;  # Mark the random number as used
+    
+    print SAIDA ">OTU$random_number\n$sequence\n";
 }
+
 close(SAIDA);
 close(FASTA);
